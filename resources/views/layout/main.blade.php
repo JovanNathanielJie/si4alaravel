4fe8877d38dafc0a7c3221620b3c27dee8168f42
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2025-05-07 14:56:23 +0700

    update adminlte4

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,7 +3,7 @@
   <!--begin::Head-->
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>AdminLTE 4 | Unfixed Sidebar</title>
+    <title>AdminLTE 4 | @yield('title')</title>
     <!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="title" content="AdminLTE 4 | Unfixed Sidebar" />
@@ -42,7 +42,7 @@
     />
     <!--end::Third Party Plugin(Bootstrap Icons)-->
     <!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="{{ asset('css/adminlte.css')}}" />
+    <link rel="stylesheet" href=" {{ asset('css/adminlte.css')}}" />
     <!--end::Required Plugin(AdminLTE)-->
   </head>
   <!--end::Head-->
@@ -112,7 +112,7 @@
                   <div class="d-flex">
                     <div class="flex-shrink-0">
                       <img
-                        src="{{ asset('assets/img/user8-128x128.jpg') }}"
+                        src="{{ asset('assets/img/user8-128x128.jpg')}}"
                         alt="User Avatar"
                         class="img-size-50 rounded-circle me-3"
                       />
@@ -257,7 +257,7 @@
           <a class='brand-link' href='/dist/pages/'>
             <!--begin::Brand Image-->
             <img
-              src="{{ asset('assets/img/AdminLTELogo.png')}}"
+              src="{{asset('assets/img/AdminLTELogo.png')}}"
               alt="AdminLTE Logo"
               class="brand-image opacity-75 shadow"
             />
@@ -724,11 +724,11 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-              <div class="col-sm-6"><h3 class="mb-0">Unfixed Layout</h3></div>
+              <div class="col-sm-6"><h3 class="mb-0">@yield('title')</h3></div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-end">
                   <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Unfixed Layout</li>
+                  <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
                 </ol>
               </div>
             </div>
@@ -741,42 +741,7 @@
         <div class="app-content">
           <!--begin::Container-->
           <div class="container-fluid">
-            <!--begin::Row-->
-            <div class="row">
-              <div class="col-12">
-                <!-- Default box -->
-                <div class="card">
-                  <div class="card-header">
-                    <h3 class="card-title">Title</h3>
-                    <div class="card-tools">
-                      <button
-                        type="button"
-                        class="btn btn-tool"
-                        data-lte-toggle="card-collapse"
-                        title="Collapse"
-                      >
-                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-tool"
-                        data-lte-toggle="card-remove"
-                        title="Remove"
-                      >
-                        <i class="bi bi-x-lg"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <div class="card-body">Start creating your amazing application!</div>
-                  <!-- /.card-body -->
-                  <div class="card-footer">Footer</div>
-                  <!-- /.card-footer-->
-                </div>
-                <!-- /.card -->
-              </div>
-            </div>
-            <!--end::Row-->
+            @yield('content')
           </div>
         </div>
         <!--end::App Content-->
@@ -801,8 +766,8 @@
     <!--begin::Script-->
     <!--begin::Third Party Plugin(OverlayScrollbars)-->
     <script
-      src="{https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
-      integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ=}"
+      src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
+      integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
       crossorigin="anonymous"
     ></script>
     <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,30 +1,64 @@
 @extends('layout.main')
+@section('title','Mahasiswa')
 
 @section('content')
-<h1>Mahasiswa</h1>
-
-<table>
-    <tr>
-        <th>NPM</th>
-        <th>Nama</th>
-        <th>Jenis Kelamin</th>
-        <th>Tanggal Lahir</th>
-        <th>Tempat Lahir</th>
-        <th>Asal SMA</th>
-        <th>Prodi</th>
-        <th>Foto</th>
-    </tr>
-    @foreach ($mahasiswa as $item)
-    <tr>
-        <td>{{ $item->npm }}</td>
-        <td>{{ $item->nama}}</td>
-        <td>{{ $item->jenis_kelamin}}</td>
-        <td>{{ $item->tanggal_lahir}}</td>
-        <td>{{ $item->tempat_lahir}}</td>
-        <td>{{ $item->asal_sma}}</td>
-        <td>{{ $item->prodi->nama}}</td>
-        <td>{{ $item->foto}}</td>
-    </tr>
-    @endforeach
-</table>
-@endsection
+<!--begin::Row-->
+<div class="row">
+    <div class="col-12">
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">List Mahasiswa</h3>
+          <div class="card-tools">
+            <button
+              type="button"
+              class="btn btn-tool"
+              data-lte-toggle="card-collapse"
+              title="Collapse"
+            >
+              <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+              <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+            </button>
+            <button
+              type="button"
+              class="btn btn-tool"
+              data-lte-toggle="card-remove"
+              title="Remove"
+            >
+              <i class="bi bi-x-lg"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+            <table>
+                <tr>
+                    <th>NPM</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Tanggal Lahir</th>
+                    <th>Tempat Lahir</th>
+                    <th>Asal SMA</th>
+                    <th>Prodi</th>
+                    <th>Foto</th>
+                </tr>
+                @foreach ($mahasiswa as $item)
+                <tr>
+                    <td>{{ $item->npm }}</td>
+                    <td>{{ $item->nama}}</td>
+                    <td>{{ $item->jenis_kelamin}}</td>
+                    <td>{{ $item->tanggal_lahir}}</td>
+                    <td>{{ $item->tempat_lahir}}</td>
+                    <td>{{ $item->asal_sma}}</td>
+                    <td>{{ $item->prodi->nama}}</td>
+                    <td>{{ $item->foto}}</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+  </div>
+  <!--end::Row-->
+  @endsection
