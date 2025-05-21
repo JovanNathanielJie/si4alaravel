a96ed4ee8bfb76ce9bf91acd802135fedc506a37
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2025-05-21 14:48:52 +0700

    perbaiki prodi jadi program studi

--- resources/views/prodi/show.blade.php
+++ resources/views/prodi/show.blade.php
@@ -8,7 +8,7 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title"><b>List Prodi</b></h3>
+          <h3 class="card-title"><b>List Program Studi</b></h3>
           <div class="card-tools">
             <button
               type="button"
